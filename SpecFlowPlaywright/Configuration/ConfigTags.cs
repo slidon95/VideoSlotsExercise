8c745c19809258b8692e8c55018546746d27e938
slidon95 <132678833+slidon95@users.noreply.github.com>
2024-10-16 11:26:35 +0100

    first commit
    first commit

--- SpecFlowPlaywright/Configuration/ConfigTags.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SpecFlowPlaywright.Configuration;
-
-public class ConfiTags
-{
-    public const string UiTag = "ui";
-    public const string ScenarioContextDriver = "Driver";
-    public const string ScenarioContextPage = "Page";
-}
